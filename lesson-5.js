f8fbe561932af34b1a1f444b1364ad425509422f
João Manoel Matias Soares <joaozinpaypal123@gmail.com>
2022-02-02 18:44:55 -0300

    Exercício 5 adicionado

---
+++ lesson-5.js
@@ -0,0 +1,14 @@
+//Exercício 5: Faça um programa que retorne o maior de dois números
+
+const a = 1;
+const b = 20;
+
+if(a > b){
+    console.log("Os números a serem comparados são: " + a + " e " + b);
+    console.log("O número " + a + " é maior que " + b);
+
+}else if(a < b){
+    console.log("Os números a serem comparados são: " + b + " e " + a);
+    console.log("O número " + b + " é maior que " + a);
+    
+}
