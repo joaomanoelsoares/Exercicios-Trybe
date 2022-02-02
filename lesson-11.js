d99a00b5b8b4ca3f9f6c912a747e8ca8f5dea67d
João Manoel Matias Soares <joaozinpaypal123@gmail.com>
2022-02-02 19:19:22 -0300

    Exercício 11 adicionado

---
+++ lesson-11.js
@@ -0,0 +1,14 @@
+//Exercício onze: Um programa que defina três números em constantes e retorne true se pelo menos um deles for par
+//Desafio: Fazer isso usando apenas um if
+
+let a = 13.7;
+let b = 14.3;
+let c = 10;
+
+
+
+if(a % 2 == 0 || b % 2 == 0 || c % 2 == 0){
+    console.log("true");
+}else{
+    console.log("false");
+}
