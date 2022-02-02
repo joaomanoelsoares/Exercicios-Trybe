3a85b4b6e4108d88545282fff682a94296384f2d
João Manoel Matias Soares <joaozinpaypal123@gmail.com>
2022-02-02 18:54:58 -0300

    Sétimo exercício adicionado

---
+++ lesson-7.js
@@ -0,0 +1,13 @@
+//Exercício sete: Fazer um programa que determine se um número é positivo, negativo ou zero.
+
+let a = 0;
+
+console.log("O número sendo utilizado atualmente é: " + a);
+
+if(a < 0){
+    console.log("O número é negativo!");
+}else if(a > 0){
+    console.log("O número é positivo!")
+}else{
+    console.log("O número é zero!")
+}
